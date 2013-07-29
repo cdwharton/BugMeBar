3c7cc6dbf701c2f0e149ad3e733fd8ec64dcdda2
Chris Wharton <hello@weare2ndfloor.com>
2013-07-29 15:31:08 +0100

    v1.0.0 Initial release

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
 BugMeBar
-========
+=====
 
 A simple little annoying notification bar plugin
+
+
+Demo
+----
+[http://weare2ndfloor.com/bugme/](http://http://weare2ndfloor.com/bugmebar/)
+
+
+Dependencies
+------------
+
+* [jQuery](https://github.com/jquery/jquery)
+* [jQuery.cookie](https://github.com/carhartl/jquery-cookie)
+
+
+
+Set up
+----
+Nice and easy, just include this in your &lt;head&gt; or just before &lt;/body&gt;
+
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="jquery.cookie.js"></script>
+	<script src="jquery.bugme.js"></script>
+
+Then call the function…
+
+	<script>
+		jQuery(document).ready(function () {
+    		jQuery('body').bugme();
+	});
+	<script>
+
+Options
+---
+	target: jQuery(this), // target of where you want the bugme bar to go
+	message: "Message in here", // message that goes inside the bug me bar
+	close: true, // have the close button on or off
+	closeText: "X", // for the little dismiss cross in the top right
+	align: "left", // align the text left, right, center or justify
+	remember: true, // this stores a cookie to remember cancellation of bugme bar
+	expireIn: 7, // set expiry of remember cookie (in days)
+	colours: true, // this will traverse through the colours in the CSS - CSS3 browsers only
+	animate: true // this will animate in and out the bugme bar - CSS3 browsers only
+	
+Browsers
+---
+
+Aside from the colour changing aspect and the bouncy animation, this works in IE7 & IE8.
+
+It has also been tested in all the cool browsers (Chrome, Safari, Firefox, Opera), iOS, Windows Phone and Android Phone… and IE9 &IE10.
+
+Special thanks
+---
+**Dan Eden** for his awesome [animate.css](http://daneden.me/animate/)
+
+**Klaus Hartl** for his amazing [jquery.cookie plugin](https://github.com/carhartl/jquery-cookie)
+
+
+Why did I build this?
+---
+Because I needed something mega simple without a gazillion options that also looked nice and I couldn't find anything that matched those requirements.
+
+
+Happy annoying people, everyone :)
---
+++ jquery.bugme.js
@@ -0,0 +1,119 @@
+/**
+ * jQuery bugMe v1.0
+ * Copyright (C) 2013 Chris Wharton (chris@weare2ndfloor.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT
+ * HOLDERS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY OR
+ * FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THE SOFTWARE
+ * OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD PARTY PATENTS,
+ * COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.COPYRIGHT HOLDERS WILL NOT
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL
+ * DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://gnu.org/licenses/>.
+ *
+ * View the demo at http://weare2ndfloor.com/bugme
+ *
+ */
+(function ($) {
+    $.fn.bugme = function (options) {
+        var defaults = {
+            target: jQuery(this), // target of where you want the bugme bar to go
+            message: "Super duper exciting announcement goes in here! You can even have a link <a href='http://weare2ndfloor.com'>weare2ndfloor.com</a>", // message that goes inside the bug me bar
+            close: true, // have the close button on or off
+            closeText: "X", // for the little dismiss cross in the top right
+            align: "left", // align the text left, right, center or justify
+            remember: true, // this stores a cookie to remember cancellation of bugme bar
+            expireIn: 7, // set expiry of remember cookie (in days)
+            colours: true, // this will traverse through the colours in the CSS - CSS3 browsers only
+            animate: true // this will animate in and out the bugme bar - CSS3 browsers only
+        };
+        var options = $.extend(defaults, options);
+        
+        return this.each(function () {
+            var target = options.target;
+            var message = options.message;
+            var close = options.close;
+            var align = options.align;
+            var closeText = options.closeText;
+            var remember = options.remember;
+            var colours = options.colours;
+            var animate = options.animate;
+            var expireIn = options.expireIn;
+            
+            // lets get going
+            function bugMe() {
+            
+            	if ( close ) {
+            		closer = '<a class="bugme-close" href="#">' + closeText + '</a>';
+            	} else {
+            	    closer = "";        
+            	}
+            	
+            	if ( animate ) {
+            		animateme = ' animated bounceInDown';
+            	} else {
+            	    animateme = "";        
+            	}
+            	
+            	//check for cookie present...
+            	
+            	var bugCookieExist = jQuery.cookie('bugme') == "close";
+            	
+            	if ( !bugCookieExist ) {
+            	
+	            	jQuery(target).prepend('<div class="bugme' + animateme + '" style="text-align:'+ align +'">' + message + closer + '</div>');
+	            
+		            if ( colours ) {
+						
+						var timeoutId;
+						function addColourTimeout() {
+							timeoutId = window.setTimeout(addColour, 2000);
+						}
+						function addColour() {
+							jQuery('.bugme').addClass("colours");
+						}
+						addColourTimeout();
+		            	
+		            } 
+		            
+		            jQuery('a.bugme-close').on( "click" , function (e) {
+		            	
+		            	e.preventDefault();
+		            	jQuery('.bugme').removeClass("colours").addClass("bounceOutUp");
+		            	
+		            	if ( remember ) {
+		            		jQuery.cookie('bugme', 'close', { expires: expireIn, path: '/' });
+		            	}
+		            	
+		            	var timeoutId;
+		            	function bodyUpTimeout() {
+		            		timeoutId = window.setTimeout(bodyUp, 1000);
+		            		timeoutId = window.setTimeout(bugMeRemove, 2000);
+		            	}
+		            	function bodyUp() {
+		            		jQuery('.bugme').slideUp();
+		            	}
+		            	function bugMeRemove() {
+		            		jQuery('.bugme').remove();
+		            	}
+		            	bodyUpTimeout();
+		            	
+		            });
+	            
+	            }
+	            
+            }            
+            
+           // run main bugMe function on load
+           bugMe(); 
+        });
+    };
+})(jQuery);
